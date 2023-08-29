42f469bdb55f8d1499a5660d2b33043d65acd849
Jiahuan Shi <403639272@qq.com>
2023-08-29 14:57:35 +0800

    feat: 添加 redisSet 测试

--- internal/biz/demo.go
+++ internal/biz/demo.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-redis/redis/v8"
 	pb "helloworld/api/helloworld/v1"
+	"sync"
 )
 
 type DemoRepo interface {
@@ -15,6 +16,7 @@ type DemoRepo interface {
 type DemoUsecase struct {
 	repo DemoRepo
 	log  *log.Helper
+	once sync.Once
 }
 
 func NewDemoUsecase(repo DemoRepo, logger log.Logger) *DemoUsecase {
@@ -23,17 +25,19 @@ func NewDemoUsecase(repo DemoRepo, logger log.Logger) *DemoUsecase {
 
 func (uc *DemoUsecase) Test(ctx context.Context, r *pb.TestRequest) (res string, err error) {
 	switch r.Type {
-	case "test1":
+	case "testRedisHash":
 		res, err = uc.TestRedisHash(ctx, r)
-	case "test2":
+	case "testRedisSet":
 		res, err = uc.TestRedisSet(ctx, r)
+	case "testRedisList":
+		res, err = uc.TestRedisList(ctx, r)
 	default:
 		return "请检查传入参数 type 设置", nil
 	}
 	return res, err
 }
 
-func (uc *DemoUsecase) TestRedisHash(ctx context.Context, r *pb.TestRequest) (res string, err error) {
+func (uc *DemoUsecase) TestRedisHash(ctx context.Context, _ *pb.TestRequest) (res string, err error) {
 	rdb, _ := uc.repo.GetRDB()
 
 	// - 联通测试
@@ -51,16 +55,34 @@ func (uc *DemoUsecase) TestRedisHash(ctx context.Context, r *pb.TestRequest) (re
 	fmt.Println(keys)
 	data := rdb.HScan(ctx, "hashMapData:1", 0, "*1", 10)
 	fmt.Println("get data: ===> ", data)
-
-	// - list
-	rdb.LPush(ctx, "test", r.Type)
 	return res, err
 }
 
-func (uc *DemoUsecase) TestRedisSet(ctx context.Context, r *pb.TestRequest) (res string, err error) {
+func (uc *DemoUsecase) TestRedisSet(ctx context.Context, _ *pb.TestRequest) (res string, err error) {
 	rdb, _ := uc.repo.GetRDB()
 
-	// - set
-	rdb.SAdd(ctx, "test", r.Type)
+	// - 添加元素
+	rdb.SAdd(ctx, "test:set:1", "rika", "boqi", "hitori", "hikari", "hanata", "narida")
+	rdb.SAdd(ctx, "test:set:2", "boqi", "hanada", "narida")
+
+	// 差集, 注意是集合 1 和其他集合的差集, 有顺序关系
+	rdb.SDiffStore(ctx, "test:set:3", "test:set:1", "test:set:2")
+
+	// 交集
+	rdb.SInterStore(ctx, "test:set:4", "test:set:1", "test:set:2")
+
+	// 并集
+	rdb.SUnionStore(ctx, "test:set:5", "test:set:1", "test:set:2")
+	return res, err
+}
+
+func (uc *DemoUsecase) TestRedisList(ctx context.Context, r *pb.TestRequest) (res string, err error) {
+	rdb, _ := uc.repo.GetRDB()
+
+	// - list, 这里使用 sync once 来确保启动程序之后一次只添加一次数据
+	uc.once.Do(func() {
+		rdb.LPush(ctx, "test", r.Type)
+	})
+
 	return res, err
 }
